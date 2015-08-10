f9068e566610eae19ee92d8f864859be72ff227d
mvladm14 <vlad.mirel@outlook.com>
2015-08-10 09:39:01 +0200

    simple CRUD on database

---
+++ howyoulikeit/Models/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace howyoulikeit.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+}
--- howyoulikeit/Models/MyDbContext.cs
+++ howyoulikeit/Models/MyDbContext.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 
 namespace howyoulikeit.Models
 {
     public class MyDbContext : DbContext
     {
         public MyDbContext() : base("name=DefaultConnection") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Post>().Property(h => h.Title).HasMaxLength(200).IsRequired();
+            modelBuilder.Entity<Blog>().Property(h => h.Name).HasMaxLength(200).IsRequired();
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
     }
 }
